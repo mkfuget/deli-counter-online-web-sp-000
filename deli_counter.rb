7009cc91f358f1d612c3c4055cbaff04a7a0e911
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:57:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(deli)
   if(deli.size == 0)
     puts "The line is currently empty."
   else 
-    # puts "The line is currently"
-    deli.each_with_index { |customer, index| puts "#{i+1}. #{deli[i]}" }
+    puts "The line is currently"
+    deli.each_with_index { |customer, index| puts "#{index+1}. #{deli[index]}" }
   end
       
 end  
