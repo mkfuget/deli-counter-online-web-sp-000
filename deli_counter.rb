265faa5f12cbf7933d8258ea4d543bd31332d042
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 08:03:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,4 +21,8 @@ def now_serving(deli)
   if(deli.size == 0)
     puts "There is nobody waiting to be served!"
   else
-    puts 
+    puts "Currently serving #{deli[0}."
+    deli.shift
+    return deli
+  end
+  
