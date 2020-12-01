d2da4ce26b4e620001fac66181be237e07b33842
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 07:53:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(deli)
   if(katz_deli.size == 0)
     puts "The line is currently empty."
   else 
-    katz_deli.each do |customer|
+    katz_deli.each_with_index { |customer, index| 
       
 end  
